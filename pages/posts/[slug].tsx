2ac1c7691a5cb31c94c49386a18d0bebcbdd8942
Bassel Kanso <basselkanso82@gmail.com>
2021-05-02 09:54:24 +0300

    fix post

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -36,7 +36,7 @@ type PostPageProps = {
 const PostPage = ({ source, frontMatter }: PostPageProps): JSX.Element => {
   const content = hydrate(source, { components });
   const customMeta: MetaProps = {
-    title: `${frontMatter.title} - Hunter Chang`,
+    title: `${frontMatter.title} - Bassel Kanso`,
     description: frontMatter.description,
     image: `${WEBSITE_HOST_URL}${frontMatter.image}`,
     date: frontMatter.date,
